27103fce709ec806b5067ad03addc1296fb03c9a
Martin Vejmelka <vejmelkam@gmail.com>
2016-04-06 18:32:55 -0600

    added matplotlib to mock objects for RTD

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -25,7 +25,7 @@ class Mock(MagicMock):
     def __getattr__(cls, name):
             return Mock()
 
-MOCK_MODULES = ['netCDF4', 'numpy', 'pyproj', 'mpl_toolkits']
+MOCK_MODULES = ['netCDF4', 'numpy', 'pyproj', 'mpl_toolkits', 'matplotlib']
 sys.modules.update((mod_name, Mock()) for mod_name in MOCK_MODULES)
 
 
