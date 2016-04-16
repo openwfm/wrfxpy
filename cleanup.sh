af2066dd8a0bf3d813ac22ab2cfec921d7d20294
Martin Vejmelka <vejmelkam@gmail.com>
2016-04-15 19:44:32 -0600

    added scripts that help clean up visualizations on remote server

---
+++ cleanup.sh
@@ -0,0 +1,3 @@
+ #!/usr/bin/env bash
+PYTHONPATH=src
+python src/cleanup.py $1 $2
---
+++ src/cleanup.py
@@ -0,0 +1,102 @@
+# Copyright (C) 2013-2016 Martin Vejmelka, UC Denver
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies
+# of the Software, and to permit persons to whom the Software is furnished to do
+# so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED,
+# INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR
+# A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+# HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION
+# OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
+# SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+
+from ssh_shuttle import SSHShuttle
+import json
+import os
+import sys
+import logging
+
+
+def retrieve_catalog(cfg):
+    """
+    Retrieve the catalog from the remote visualization server.
+    
+    :param cfg: the configuration of the remote visualization host
+    """
+    s = SSHShuttle(cfg)
+    logging.info('SHUTTLE connecting to remote host %s' % s.host)
+    s.connect()
+    logging.info('SHUTTLE retrieving catalog file.')
+    s.get('catalog.json', '_catalog.json')
+
+    cat = json.load(open('_catalog.json'))
+    s.disconnect()
+    os.remove('_catalog.json')
+    logging.info('SHUTTLE retrieve complete.')
+    return cat
+
+
+def store_catalog(cfg, cat):
+    """
+    Store a new version of the catalog on the remote server given
+    by cfg.
+
+    :param cfg: the configuration of the remote visualization host
+    :param cat: the JSON object representing the new catalog
+    """
+    s = SSHShuttle(cfg)
+    logging.info('SHUTTLE connecting to remote host %s' % s.host)
+    s.connect()
+    logging.info('SHUTTLE sending catalog file.')
+    json.dump(cat, open('_catalog.json', 'w'), indent=4, separators=(',', ': '))
+    s.put('_catalog.json', 'catalog.json')
+    os.remove('_catalog.json')
+    s.disconnect()
+    logging.info('SHUTTLE send complete.')
+
+
+def remote_rmdir(cfg, dirname):
+    s = SSHShuttle(cfg)
+    logging.info('SHUTTLE connecting to remote host %s' % s.host)
+    s.connect()
+    logging.info('SHUTTLE removing directory %s' % dirname)
+    s.rmdir(dirname)
+    logging.info('SHUTTLE delete complete.')
+    s.disconnect()
+
+
+if __name__ == '__main__':
+
+    logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
+    commands = [ 'list', 'delete' ]
+    
+    if len(sys.argv) < 2:
+        print('usage: %s [list|delete <name>]' % sys.argv[0])
+        sys.exit(1)
+
+    cfg = json.load(open('etc/conf.json'))
+
+    if sys.argv[1] == 'list':
+        cat = retrieve_catalog(cfg)
+        for k in cat:
+            print(k)
+    elif sys.argv[1] == 'delete':
+        cat = retrieve_catalog(cfg)
+        if sys.argv[2] not in cat:
+            print('Cannot find simulation %s' % sys.argv[2])
+        else:
+            del cat[sys.argv[2]]
+            store_catalog(cfg, cat)
+            remote_rmdir(cfg, sys.argv[2])
+    else:
+        logging.error('command line not understood %s' % sys.argv)
+        
+
