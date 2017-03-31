b5e87761ad9504e063397ea914ee854e8cddc69e
Dalton Burke <dmtburke@gmail.com>
2017-03-30 19:28:04 -0600

    made level0 retrieval test more pythonic

--- l0_retr_test.py
+++ l0_retr_test.py
@@ -6,51 +6,47 @@
 # Test correct functionality of the retrieval of level0 files
 
 import subprocess
-import shlex
 import datetime
+import shutil
 
+local_path = 'l0_test_ingest'
+
+# Remove data from old tests
+shutil.rmtree(local_path, ignore_errors=True)
 
 current_time = datetime.datetime.utcnow()
+
 ten_hours_ago = str(current_time - datetime.timedelta(hours=10)).replace(' ', '_')
 five_hours_ago = str(current_time - datetime.timedelta(hours=5)).replace(' ', '_')
 current_time = str(current_time).replace(' ', '_')
 
-print "\nRETRIEVING AQUA FILES FROM THE LAST 5 HOURS WITH CALL:"
-print ('./level0_retr.sh %s %s %s %s \n' %
-       ('MODIS_AQUA', five_hours_ago, current_time, '~/level0test/ingest'))
+source_types = ['MODIS_AQUA', 'MODIS_TERRA', 'VIIRS_NPP']
 
-subprocess.call(['./level0_retr.sh', 'MODIS_AQUA', five_hours_ago, current_time, '~/level0test/ingest'])
+# -----------------------------------------------------------------------
+# Download all data sources from the last 5 hours
 
-print "\nRETRIEVING TERRA FILES FROM THE LAST 5 HOURS WITH CALL"
-print ('./level0_retr.sh %s %s %s %s\n' %
-       ('MODIS_TERRA', five_hours_ago, current_time, '~/level0test/ingest'))
+print "TESTING SOURCES FOR FILES IN LAST 5 HOURS\n"
 
-subprocess.call(['./level0_retr.sh', 'MODIS_TERRA', five_hours_ago, current_time, '~/level0test/ingest'])
+for t in source_types:
+    print "\nRETRIEVING %s FILES FROM THE LAST 5 HOURS WITH CALL:" % t
+    print './level0_retr.sh %s %s %s %s \n' %
+           (t, five_hours_ago, current_time, local_path)
 
-print "\nRETRIEVING VIIRS FILES FROM THE LAST 5 HOURS WITH CALL:"
-print ('./level0_retr.sh %s %s %s %s' %
-       ('VIIRS_NPP', five_hours_ago, current_time, '~/level0test/ingest'))
-
-subprocess.call(['./level0_retr.sh', 'VIIRS_NPP', five_hours_ago, current_time, '~/level0test/ingest'])
+    subprocess.call(['./level0_retr.sh', t, five_hours_ago, current_time, local_path])
 
 print "\nDONE RETRIEVING FILES FROM LAST 5 HOURS \n\n"
 
-print "\nRETRIEVING AQUA FILES FROM LAST 10 HOURS (We should already have some) WITH CALL:"
-print ('./level0_retr.sh %s %s %s %s\n' %
-       ('MODIS_AQUA', ten_hours_ago, current_time, '~/level0test/ingest'))
+# -----------------------------------------------------------------------
+# Download all data sources from the last 10 hours
+# (some data we should already have, so those should be skipped)
 
-subprocess.call(['./level0_retr.sh', 'MODIS_AQUA', ten_hours_ago, current_time, '~/level0test/ingest'])
+print "TESTING SOURCES FOR FILES IN LAST 10 HOURS\n"
 
-print "\nRETRIEVING TERRA FILES FROM LAST 10 HOURS (We should already have some) WITH CALL:"
-print ('./level0_retr.sh %s %s %s %s\n' %
-       ('MODIS_TERRA', ten_hours_ago, current_time, '~/level0test/ingest'))
+for t in source_types:
+    print "\nRETRIEVING %s FILES FROM THE LAST 10 HOURS WITH CALL:" % t
+    print './level0_retr.sh %s %s %s %s \n' %
+           (t, ten_hours_ago, current_time, local_path)
 
-subprocess.call(['./level0_retr.sh', 'MODIS_TERRA', ten_hours_ago, current_time, '~/level0test/ingest'])
-
-print "\nRETRIEVING VIIRS FILES FROM LAST 10 HOURS (We should already have some) WITH CALL:"
-print ('./level0_retr.sh %s %s %s %s\n' %
-       ('VIIRS_NPP', ten_hours_ago, current_time, '~/level0test/ingest'))
-
-subprocess.call(['./level0_retr.sh', 'VIIRS_NPP', ten_hours_ago, current_time, '~/level0test/ingest'])
+    subprocess.call(['./level0_retr.sh', t, ten_hours_ago, current_time, local_path])
 
 print "\nDONE RETRIEVING FILES FROM LAST 10 HOURS"
