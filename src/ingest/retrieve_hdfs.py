edefe94fc828dd04167ef391d28179ec7e651ad6
Jan Mandel <jan.mandel@gmail.com>
2017-08-29 15:25:40 -0600

    fixing retrieve_hdfs.py

--- src/ingest/retrieve_hdfs.py
+++ src/ingest/retrieve_hdfs.py
@@ -13,7 +13,7 @@ import os.path as osp
 ## Standalone script that can be used to download files
 if __name__ == '__main__':
     if len(sys.argv) != 9:
-        print('Usage: %s <hdf_source_name> <esmf_from_utc> <esmf_to_utc> <low_long> <high_long> <low_lat> <high_lat> <target_directory>' % sys.argv[0])
+        print('Usage: %s <data_src_name> <esmf_from_utc> <esmf_to_utc> <low_long> <high_long> <low_lat> <high_lat> <target_directory>' % sys.argv[0])
         print('        supported HDF sources: MODIS_AQUA, MODIS_TERRA, VIIRS_NPP')
         print('        time of format YYYY.MM.DD-HH.MM.SS')
         sys.exit(-1)
@@ -28,17 +28,23 @@ if __name__ == '__main__':
     lonlat = [float(sys.argv[4]), float(sys.argv[5]), float(sys.argv[6]), float(sys.argv[7])]
     ingest_dir = osp.abspath(sys.argv[8])
 
+    logging.info('data_src_name: %s' % data_src_name)
+    logging.info('from_utc:      %s' % from_utc)
+    logging.info('to_utc:        %s' % to_utc)
+    logging.info('lonlat:        %s:%s %s:%s' % tuple(lonlat))
+    logging.info('ingest_dir:    %s' % ingest_dir)
+
     hdf_src = None
     if data_src_name == 'MODIS_AQUA':
         data_src = MODIS_AQUA(ingest_dir)
-    if data_src_name == 'MODIS_TERRA':
+    elif data_src_name == 'MODIS_TERRA':
         data_src = MODIS_TERRA(ingest_dir)
     elif data_src_name == 'VIIRS_NPP':
         data_src = VIIRS_NPP(ingest_dir)
     else:
         raise ValueError('Invalid HDF source %s' % data_src_name)
 
-    logging.info('Initiating download of files from HDF source %s' % grib_src_name)
+    logging.info('Initiating download of files from HDF source %s' % data_src_name)
 
     hdfs = data_src.retrieve_data(from_utc, to_utc, lonlat)
 
