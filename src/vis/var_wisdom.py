98480bd7ddc3f08d40a73313feff7137af7c50b2
Jan Mandel <jan.mandel@gmail.com>
2018-07-29 12:43:28 -0600

    splitting off vis_utils

--- src/vis/var_wisdom.py
+++ src/vis/var_wisdom.py
@@ -1,73 +1,10 @@
 import numpy as np
 import logging
 
+from vis.vis_utils import interpolate2height, index8height
+
 smoke_threshold_int = 50
 smoke_threshold = 10
-
-def interpolate2height_old(var,height,level):
-      """
-      Interpolate 3d variable to a given height
-      :param var: the 3d array to be interpolated, 1st axis is vertical
-      :param height: the 3d array of heights of the nodes on which v is defined
-      :param level: the target height to interpolate to
-      :return: interpolated value, or
-      """
-      maxlayer = var.shape[0]-1
-      z = np.zeros([var.shape[1],var.shape[2]])
-      r = z
-      for i in range(0, var.shape[1]):
-          for j in range(0, var.shape[2]):
-             k = np.searchsorted(height[:,i,j],level)
-             if k==0 or k>maxlayer:
-                 logging.error("Need height[0,%s,%s]=%s < level=%s <= height[%s,%s,%s]=%s" \
-                    % (i,j,height[0,i,j],level,maxlayer,i,j,height[maxlayer,i,j]))
-                 return z
-             # interpolate in the interval height[k-1,i,j] to height[k,i,j]
-             r[i,j]=var[k-1,i,j]+(var[k,i,j]-var[k-1,i,j]) \
-                     * (level - height[k-1,i,j])/(height[k,i,j] - height[k-1,i,j])
-      return r
-
-def interpolate2height(var,height,level):
-      """
-      Interpolate 3d variable to a given height
-      :param var: the 3d array to be interpolated, 1st axis is vertical
-      :param height: the 3d array of heights of the nodes on which v is defined
-      :param level: the target height to interpolate to
-      :return: interpolated value, or
-      """
-      ix, tx = index8height(height,level)
-      r = np.zeros([var.shape[1],var.shape[2]])
-      for i in range(0, var.shape[1]):
-          for j in range(0, var.shape[2]):
-             k = ix[i,j]
-             t = tx[i,j]
-             #r[i,j]=var[k,i,j]+(var[k+1,i,j]-var[k,i,j])*tx[i,j] 
-             r[i,j] = var[k,i,j]*(1.0-t) + var[k+1,i,j]*t 
-      return r
-
-def index8height(height,level):
-      """
-      Find index and fraction at given height
-      :param height: the 3d array of heights of the nodes on which v is defined
-      :param level: the target height to interpolate to
-      :return: interpolated value, or
-      """
-      maxlayer = height.shape[0]-1
-      iz = np.zeros([height.shape[1],height.shape[2]],dtype=np.int_)
-      dz = np.zeros([height.shape[1],height.shape[2]])
-      ix = np.copy(iz)
-      tx = np.copy(dz)
-      for i in range(0, height.shape[1]):
-          for j in range(0, height.shape[2]):
-             k = np.searchsorted(height[:,i,j],level)
-             if k==0 or k>maxlayer:
-                 logging.error("Need height[0,%s,%s]=%s < level=%s <= height[%s,%s,%s]=%s" \
-                    % (i,j,height[0,i,j],level,maxlayer,i,j,height[maxlayer,i,j]))
-                 return z, z
-             ix[i,j]=k-1    # integer part
-             # interpolation in the interval height[k-1,i,j] to height[k,i,j]
-             tx[i,j]= (level - height[k-1,i,j])/(height[k,i,j] - height[k-1,i,j])
-      return ix,tx 
 
 def p_height(d,t):
       """
