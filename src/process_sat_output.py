e9b6442e3e2d78b643a0b7c0a32c8a4632364698
Angel Farguell Caus <angel.farguell@gmail.com>
2019-04-22 19:24:44 -0600

    new process_sat files to process satellite data only

---
+++ src/process_sat_output.py
@@ -0,0 +1,8 @@
+from forecast import process_sat_output
+import sys, logging
+
+if len(sys.argv) < 2: 
+    raise SystemExit('usage: ./process_sat_output.sh job_id')
+
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+process_sat_output(sys.argv[1])
