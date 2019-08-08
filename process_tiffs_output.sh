6bd08c5110e1a7d31d560e5c659e76fa59867416
Angel Farguell Caus <angel.farguell@gmail.com>
2019-08-08 12:34:01 -0600

    conda activate not working inside bash script

--- process_tiffs_output.sh
+++ process_tiffs_output.sh
@@ -8,5 +8,5 @@ if [ $# -eq 0 ]
 fi
 # if this fails, install conda and run:
 #   conda create -n gdal python=3.4 gdal netcdf4 jpeg=8 pyproj matplotlib
-conda activate gdal
+#   conda activate gdal
 python src/process_tiffs.py $1
