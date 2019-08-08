8db8007b669c270b5ffac4c4f03f7579e7c814d0
Angel Farguell Caus <angel.farguell@gmail.com>
2019-08-08 10:23:47 -0600

    adding VIIRS 375m product

--- src/ingest/VIIRS.py
+++ src/ingest/VIIRS.py
@@ -45,7 +45,7 @@ class SNPPHR(VIIRS):
 	id='SNPPHR'
 	info_url='https://www.nasa.gov/mission_pages/NPP/mission_overview/index.html'
 	info='High Resolution S-NPP Visible Infrared Imaging Radiometer Suite (VIIRS)'
-	prefix='VNP'
+	prefix='VNPHR'
 	pre_geo_prefix='VNP03MODLL'
 	geo_prefix='NPP_IMFTS_L1'
 	fire_prefix='VNP14IMG'
