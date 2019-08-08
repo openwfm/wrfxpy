4620ac3de89c8babbb2597965a60a9ab2793de7f
Angel Farguell Caus <angel.farguell@gmail.com>
2019-08-08 12:27:47 -0600

    conda activate added to process_output_tiffs.sh

--- process_output_tiffs.sh
+++ process_output_tiffs.sh
@@ -6,4 +6,7 @@ if [ $# -eq 0 ]
      echo usage: ./process_output.sh job_id
      exit 1
 fi
+# if this fails, install conda and run:
+#   conda create -n gdal python=3.4 gdal netcdf4 jpeg=8 pyproj matplotlib
+conda activate gdal
 python src/process_tiffs.py $1
