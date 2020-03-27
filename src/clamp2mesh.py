28d989dada23acdf7be486dccdb8d25846bd1bf2
Jan Mandel <jan.mandel@gmail.com>
2020-03-26 23:47:17 -0600

    new clamp2mesh reimagined from matlab

---
+++ src/clamp2mesh.py
@@ -0,0 +1,14 @@
+# clamp2mesh.py
+# Jan Mandel March 2020
+
+from __future__ import absolute_import
+from __future__ import print_function
+from __future__ import unicode_literals
+
+import os.path as osp
+import os
+import netCDF4 as nc4
+import numpy as np
+
+
+
