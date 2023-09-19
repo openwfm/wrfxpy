5dd00bfb96e7db8016e8dc4d3cb381fa9c740d6f
jonathonh <jonathon.hirschi@ucdenver.edu>
2023-09-18 22:22:03 -0600

    Add functionality to get value from given lon/lat for each tiff file band.
    NOTE: does not loop through times yet

--- src/ingest/build_raws_dict.py
+++ src/ingest/build_raws_dict.py
@@ -12,14 +12,37 @@ from pathlib import Path
 import pandas as pd
 import numpy as np
 import sys
+from osgeo import gdal, osr
 
 sys_cfg = Dict(json.load(open('etc/conf.json')))
+
+# DataFrame to track needed Bands for FMDA and metadata
+## Note there is a surface temp, but no surface RH. 
+## Sticking with 2m temp and rh for consistency since those are combined to EQ
+band_df_hrrr = pd.DataFrame({
+    'Band': [585, 616, 620, 628, 629, 661, 662, 663, 664],
+    'hrrr_name': ['GUST', 'TMP', 'RH', 'PRATE', 'APCP',
+                  'DSWRF', 'DLWRF', 'USWRF', 'ULWRF'],
+    'descr': ['surface Wind Speed (Gust) [m/s]',
+              '2 m Temperature [K]', 
+              '2 m Relative Humidity [%]', 
+              'surface Precip. Rate [kg/m^2/s]',
+              'surface Total Precipitation [kg/m^2]',
+              'surface Downward Short-Wave Radiation Flux [W/m^2]',
+              'surface Downward Long-Wave Rad. Flux [W/m^2]',
+              'surface Upward Short-Wave Radiation Flux [W/m^2]',
+              'Upward Long-Wave Rad. Flux [W/m^2]']
+})
+
 
 # Functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
 
 def build_raws(tstart_str, tend_str, stid, datapath, fmt = "%Y%m%d%H%M"):
     # Given times, stid, and path, return a dictionary of RAWS data fields (fm10, time, lat/lon, elevation) 
-    
+
+    print('~'*25)
+    print('Collecting RAWS station: '+stid)
+
     ## Format with datetime
     tstart = datetime.strptime(tstart_str, fmt)
     tend = datetime.strptime(tend_str, fmt)
@@ -59,10 +82,10 @@ def build_raws(tstart_str, tend_str, stid, datapath, fmt = "%Y%m%d%H%M"):
     p = osp.join(datapath, df2['File'][0])
     f = open(p, 'br')
     data = pickle.load(f)
-    
+
     ## Subset to given stid
     raws1 = data[data["STID"].eq(stid)]
-    
+
     for i in range(1, len(df2)):
         p = osp.join(datapath, df2['File'][i])
         f = open(p, 'br')
@@ -74,7 +97,7 @@ def build_raws(tstart_str, tend_str, stid, datapath, fmt = "%Y%m%d%H%M"):
     f = open(osp.join(datapath, 'stations.pkl'), 'br')
     st = pickle.load(f)
     st = st[st.index == stid]
-    
+
     dict1 = {
         'time': raws1['datetime'].to_numpy(),
         'STID' : raws1['STID'].unique()[0],
@@ -93,17 +116,109 @@ def build_atm(tstart_str, tend_str, lon, lat, atmpath, atm_source="HRRR"):
 
     return atm1
 
-def build_hrrr(tstart_str, tend_str, lon, lat, hrrrpath):
-    # NOT IMPLEMENTED YET, HERE FOR FORMATTING
+def interp_l1(x, y):
+    x = round(x)
+    y = round(y)
+    return x, y
+
+def get_val(tpath, y, x):
+    # tpath (str): absolute path to tiff file
+    ds = gdal.Open(tpath)
+    if(ds.RasterCount>1):
+        print('Not Implemented for multiple Raster bands')
+        sys.exit(-1)
+    band = ds.GetRasterBand(1)
+    data = band.ReadAsArray()
+    return data[y, x]
+
+def build_hrrr(tstart_str, tend_str, lon, lat, hrrrpath, method = 'l1'):
+    # method (str): interpolation method (NOTE: currently only L1 nearest neighbors aka rounding) 
+
+    # Read tif file
+    bandnum=585 # Start with gust Band, then reuse those projection calculations later
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    print("Opening: "+tpath)
+    ds = gdal.Open(tpath)
+    gt = ds.GetGeoTransform()
+    gp = ds.GetProjection()
+    
+    if(ds.RasterCount>1): 
+        print('Not Implemented for multiple Raster bands')
+        sys.exit(-1)
+
+    band = ds.GetRasterBand(1)
+    data = band.ReadAsArray()
+
+
+    # Get Projection info
+    point_srs = osr.SpatialReference()
+    point_srs.ImportFromEPSG(4326) # hardcode for lon/lat
+    # GDAL>=3: make sure it's x/y
+    # see https://trac.osgeo.org/gdal/wiki/rfc73_proj6_wkt2_srsbarn
+    point_srs.SetAxisMappingStrategy(osr.OAMS_TRADITIONAL_GIS_ORDER)     
+    file_srs = osr.SpatialReference()
+    file_srs.ImportFromWkt(gp)
+    ct = osr.CoordinateTransformation(point_srs, file_srs)
+
+    # Project given lon/lat to raster
+    print(f"Target lon/lat: ({lon}, {lat})")
+    mapx, mapy, z = ct.TransformPoint(np.float64(lon), np.float64(lat))
+    gt_inv = gdal.InvGeoTransform(gt)
+    pixel_x, pixel_y = gdal.ApplyGeoTransform(gt_inv, mapx, mapy)
+    print(f"Projected pixel: ({pixel_x}, {pixel_y})")
+
+    # Interpolate
+    if(method == 'l1'): 
+        x, y = interp_l1(pixel_x, pixel_y)
+    print(f"Interpolated pixel: ({x}, {y})")
+    print(data.shape)
+    wind_speed = data[y, x] ## NOTE lat/lon order flipped
+    
+    # Get other fields
+    bandnum = 616 # temp
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    temp = get_val(tpath, y, x)
+
+    bandnum = 620 # rh
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    rh = get_val(tpath, y, x)
+    
+    bandnum = 629 # accumulated precip
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    rain = get_val(tpath, y, x)
+
+    bandnum = 661
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    solarDS = get_val(tpath, y, x)
+
+    bandnum = 662
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    solarDL = get_val(tpath, y, x)
+
+    bandnum = 663
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    solarUS = get_val(tpath, y, x)
+
+    bandnum = 664
+    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
+    solarUL = get_val(tpath, y, x)
+
+    # calculate Equilibriums from rh and temp
+    Ed = 0.924*rh**0.679 + 0.000499*np.exp(0.1*rh) + 0.18*(21.1 + 273.15 - temp)*(1 - np.exp(-0.115*rh))
+    Ew = 0.618*rh**0.753 + 0.000454*np.exp(0.1*rh) + 0.18*(21.1 + 273.15 - temp)*(1 - np.exp(-0.115*rh))
+    
 
     hrrr1 = {
-        'rain' : 'RAIN',
-        'rh' : 'RH',
-        'temp' : 'TEMP',
-        'Ed' : 'ED',
-        'Ew' : 'EW',
-        'wind_speed' : 'WIND',
-        'solar' : 'SOLAR',
+        'rain' : rain,
+        'rh' : rh,
+        'temp' : temp,
+        'Ed' : Ed,
+        'Ew' : Ew,
+        'wind_speed' : wind_speed,
+        'solarDS' : solarDS,
+        'solarDL' : solarDL,
+        'solarUS' : solarUS,
+        'solarUL' : solarUL,
     }
     
     return hrrr1
@@ -114,13 +229,16 @@ def build_dictionary(tstart_str, tend_str, stid, rawspath, atmpath, dict_name = 
     atm1 = build_atm(tstart_str, tend_str, dict1['lon'], dict1['lat'], atmpath)
     
     dict1['id'] = dict_name
-    dict1['rain'] = atm1['rain'],
-    dict1['rh'] = atm1['rh'],
-    dict1['temp'] = atm1['temp'],
-    dict1['Ed'] = atm1['Ed'],
-    dict1['Ew'] = atm1['Ew'],
-    dict1['wind'] = atm1['wind_speed'],
-    dict1['solar'] = atm1['solar']
+    dict1['rain'] = atm1['rain']
+    dict1['rh'] = atm1['rh']
+    dict1['temp'] = atm1['temp']
+    dict1['Ed'] = atm1['Ed']
+    dict1['Ew'] = atm1['Ew']
+    dict1['wind'] = atm1['wind_speed']
+    dict1['solarDS'] = atm1['solarDS']
+    dict1['solarDL'] = atm1['solarDL']
+    dict1['solarUS'] = atm1['solarUS']
+    dict1['solarUL'] = atm1['solarUL']
 
     return dict1
 
@@ -130,6 +248,7 @@ if __name__ == '__main__':
 
     if len(sys.argv) != 8:
         print(('Usage: %s <esmf_from_utc> <esmf_to_utc> <STID> <rawspath> <atmpath> <target_directory> <filename>' % sys.argv[0]))
+        print("Example: python src/ingest/build_raws_dict.py 202106070800 202106300900 KAFF /storage/math/NSF1/jmandel/RAWSData/haguespeak/meso /home/hirschij/data/hrrr/geotiff_files/ ~ testfile.json")
         sys.exit(-1)
     start = sys.argv[1]
     end = sys.argv[2]
