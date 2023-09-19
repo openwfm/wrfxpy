4c951ff6aeb43dccd31ea51356e8e27c1ea77cb5
jonathonh <jonathon.hirschi@ucdenver.edu>
2023-09-18 23:44:48 -0600

    Add time looping over bands to build time series.
    Note: very slow in tests

--- src/ingest/build_raws_dict.py
+++ src/ingest/build_raws_dict.py
@@ -121,20 +121,34 @@ def interp_l1(x, y):
     y = round(y)
     return x, y
 
-def get_val(tpath, y, x):
-    # tpath (str): absolute path to tiff file
-    ds = gdal.Open(tpath)
-    if(ds.RasterCount>1):
-        print('Not Implemented for multiple Raster bands')
-        sys.exit(-1)
-    band = ds.GetRasterBand(1)
-    data = band.ReadAsArray()
-    return data[y, x]
 
-def build_hrrr(tstart_str, tend_str, lon, lat, hrrrpath, method = 'l1'):
+def build_hrrr(tstart_str, tend_str, lon, lat, hrrrpath, method = 'l1', fmt = "%Y%m%d%H%M"):
     # method (str): interpolation method (NOTE: currently only L1 nearest neighbors aka rounding) 
+    # Internal Functions, will only reasonably be used within here
 
-    # Read tif file
+    def get_vals(tpath, pixel_x, pixel_y, method='l1'):
+        # tpath (str): absolute path to tiff file
+        ds = gdal.Open(tpath)
+        if(ds.RasterCount>1):
+            print('Not Implemented for multiple Raster bands')
+            sys.exit(-1)
+        band = ds.GetRasterBand(1)
+        data = band.ReadAsArray()
+        
+        # Interpolate
+        if method == "l1":
+            x, y = interp_l1(pixel_x, pixel_y)
+        
+        return data[y, x]
+    
+
+    # Get dates
+    tstart = datetime.strptime(tstart_str, fmt)
+    tend = datetime.strptime(tend_str, fmt)
+    dates = pd.date_range(start=tstart,end=tend, freq="1H")
+    dates = dates[0:100] ### NOTE: THIS LINE IS ONLY FOR TESTING
+
+    # Assemble time series
     bandnum=585 # Start with gust Band, then reuse those projection calculations later
     tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
     print("Opening: "+tpath)
@@ -145,9 +159,6 @@ def build_hrrr(tstart_str, tend_str, lon, lat, hrrrpath, method = 'l1'):
     if(ds.RasterCount>1): 
         print('Not Implemented for multiple Raster bands')
         sys.exit(-1)
-
-    band = ds.GetRasterBand(1)
-    data = band.ReadAsArray()
 
 
     # Get Projection info
@@ -167,41 +178,44 @@ def build_hrrr(tstart_str, tend_str, lon, lat, hrrrpath, method = 'l1'):
     pixel_x, pixel_y = gdal.ApplyGeoTransform(gt_inv, mapx, mapy)
     print(f"Projected pixel: ({pixel_x}, {pixel_y})")
 
-    # Interpolate
-    if(method == 'l1'): 
-        x, y = interp_l1(pixel_x, pixel_y)
-    print(f"Interpolated pixel: ({x}, {y})")
-    print(data.shape)
-    wind_speed = data[y, x] ## NOTE lat/lon order flipped
+    
+    band = ds.GetRasterBand(1)
+    data = band.ReadAsArray()
+    
+    # Get wind (separate to not reload/transform data)
+    wind_speed = np.zeros(len(dates)) # Array filled in loop
+    for i in range(0, len(dates)):
+        d = dates[i]
+        day_file = d.strftime("%Y%m%d") # HRRR data stash is in this format
+        hour = d.strftime("%H")
+        tpath = osp.join(hrrrpath, day_file, f"hrrr.t{hour}z.wrfprsf00.{bandnum}.tif")
+        wind_speed[i] = get_vals(tpath, pixel_x, pixel_y)
     
     # Get other fields
-    bandnum = 616 # temp
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    temp = get_val(tpath, y, x)
+    # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    def build_timeseries(bandnum):
+        loop_arr = np.zeros(len(dates)) # Array filled in loop
+        for i in range(0, len(dates)):
+            d = dates[i]
+            day_file = d.strftime("%Y%m%d") # HRRR data stash is in this format
+            hour = d.strftime("%H")
+            tpath = osp.join(hrrrpath, day_file, f"hrrr.t{hour}z.wrfprsf00.{bandnum}.tif")
+            loop_arr[i] = get_vals(tpath, pixel_x, pixel_y) 
+        return loop_arr
 
-    bandnum = 620 # rh
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    rh = get_val(tpath, y, x)
+    temp = build_timeseries(616) # temp
+
+    rh = build_timeseries(620) # rh
     
-    bandnum = 629 # accumulated precip
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    rain = get_val(tpath, y, x)
+    rain = build_timeseries(629) # accumulated precip
 
-    bandnum = 661
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    solarDS = get_val(tpath, y, x)
+    solarDS = build_timeseries(661) # downward short wave solar
 
-    bandnum = 662
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    solarDL = get_val(tpath, y, x)
+    solarDL = build_timeseries(662) # downward long wave solar
 
-    bandnum = 663
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    solarUS = get_val(tpath, y, x)
+    solarUS = build_timeseries(663) # upward short wave solar
 
-    bandnum = 664
-    tpath = osp.join(hrrrpath, "20210304",f"hrrr.t00z.wrfprsf00.{bandnum}.tif")
-    solarUL = get_val(tpath, y, x)
+    solarUL = build_timeseries(664) # upward long wave solar
 
     # calculate Equilibriums from rh and temp
     Ed = 0.924*rh**0.679 + 0.000499*np.exp(0.1*rh) + 0.18*(21.1 + 273.15 - temp)*(1 - np.exp(-0.115*rh))
