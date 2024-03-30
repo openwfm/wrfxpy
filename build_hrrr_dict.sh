5dba6d920cd473c916a164d850fef1212c43a3f3
jonathonh <jonathon.hirschi@ucdenver.edu>
2024-03-30 13:56:35 -0600

    Delete shell files associated with deleted scripts

--- build_hrrr_dict.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname "$0")
-export PYTHONPATH=src
-python src/ingest/build_hrrr_dict.py $1 $2 $3 $4 $5 $6
--- build_raws_dict.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-cd $(dirname "$0")
-export PYTHONPATH=src
-python src/ingest/build_raws_dict.py $1 $2 $3 $4 
-
