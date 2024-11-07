a79e13c503f23e399187bd26ed8aa88943390e72
Jan Mandel <jan.mandel@gmail.com>
2024-11-06 17:08:31 -0700

    fixing	modified:   schedule_forecasts.py

--- schedule_forecasts.py
+++ schedule_forecasts.py
@@ -4,10 +4,10 @@ from concurrent.futures import ThreadPoolExecutor, as_completed
 from run_forecast import create_forecast_job
 
 # Define the time range
-start_date = datetime(2011, 1, 1)
+start_date = datetime(2012, 12, 7)
 end_date = datetime(2024, 11, 1)
 interval = timedelta(days=1)
-max_concurrent_jobs = 5
+max_concurrent_jobs = 9
 
 def generate_date_ranges(start, end, interval):
     current = start
@@ -24,9 +24,8 @@ if __name__ == "__main__":
         for start, end in generate_date_ranges(start_date, end_date, interval):
             futures.append(executor.submit(create_forecast_job, start, end))
         
-        # Wait for all jobs to complete
+        # Wait for all job1s to complete
         for future in as_completed(futures):
-            print(f"Job {unique_id} started.   Job JSON: {output_json}, Log: {log_file}")
             unique_id, output_json, log_file = future.result()
             print(f"Job {unique_id} completed. Job JSON: {output_json}, Log: {log_file}")
 
