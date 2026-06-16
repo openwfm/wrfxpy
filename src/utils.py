a17e1ba2805e794e5095af5cceadca17c708795f
Jan Mandel <jan.mandel@gmail.com>
2026-06-16 16:46:24 -0600

    import timezone modified:   src/utils.py

--- src/utils.py
+++ src/utils.py
@@ -17,7 +17,7 @@
 # OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE
 # SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-from datetime import datetime, timedelta
+from datetime import datetime, timedelta, timezone
 import pytz
 import os
 import os.path as osp
