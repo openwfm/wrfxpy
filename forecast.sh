b469b4bc4a195640c429126a5fbe0df9b5d473be
Angel Farguell Caus <angel.farguell@gmail.com>
2026-07-26 19:33:33 -0700

    change options for more stable simulations

--- forecast.sh
+++ forecast.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 if [ $# -eq 0 ]
   then
-     echo usage: ./forecast.sh input.json
-     exit 1
+      echo usage: ./forecast.sh input.json
+      exit 1
 fi
 cd $(dirname "$0")
 export PYTHONPATH=src
